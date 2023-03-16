65d036e5b23a0fb8eca164cae019ba015c2232d6
Gabriel Pozato <gabriel.pozato@etec.sp.gov.br>
2023-03-16 20:37:55 -0300

    adicionando mais metodos

--- backend/Contracts/IOwnerRepository.cs
+++ backend/Contracts/IOwnerRepository.cs
@@ -1,5 +1,8 @@
 using Entities.Models;
 namespace Contracts;
+
 public interface IOwnerRepository : IRepositoryBase<Owner>
 {
+    IEnumerable<Owner> GetAllOwners();
+    Owner GetOwnerById(Guid ownerId);
 }
