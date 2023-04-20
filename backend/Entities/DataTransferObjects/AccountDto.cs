b330d016f621cd00fcc22eb87a5a8712f5d2803a
Gabriel Pozato <gabriel.pozato@etec.sp.gov.br>
2023-04-20 19:28:47 -0300

    att comit

--- backend/Entities/DataTransferObjects/AccountDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Entities.DataTransferObjects
-{
-    public class AccountDto
-    {
-        public Guid Id { get; set; }
-        public DateTime DateCreated { get; set; }
-        public string AccountType { get; set; }
-    }
-}
